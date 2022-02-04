94929e707d4af704f0b1c31317f68db7621a49c5
adithkkn <95184827+adithkkn@users.noreply.github.com>
2022-02-03 22:50:40 -0800

    Add files via upload

---
+++ railways.js
@@ -0,0 +1,22 @@
+var myapp=angular.module("myapp", []);
+myapp.controller("myctrl", function($scope, $http)
+{
+    $http.get('Booking.json')
+    .success(function(response)
+    {
+        $scope.names=response.records;
+    });
+});
+
+myapp.filter("bookingstatuss", function(){
+  return function (bookingstatus){
+      switch(bookingstatus){
+          case 1:
+              return "successful";
+          case 2:
+              return "failed";
+          case 3:
+              return "pending";    
+      }
+  }
+});
